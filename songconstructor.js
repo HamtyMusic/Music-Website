53ba3adbe4c58c362bc06e61fdf8efc910b00740
ItsHamty <contact.hamty@gmail.com>
2016-10-04 17:36:55 +0300

    Update songconstructor.js

--- songconstructor.js
+++ songconstructor.js
@@ -115,7 +115,7 @@ function drawSongs(Songs) {
   }
   for (var i in Songs) {
     var Song = Songs[i];
-    var elem = newElem("div", songParent, "song");
+    var elem = newElem("div", songParent, "song pb shadow");
     if (Song.img) {
       var imgPar = newElem("div", elem, "songImgPar")
       var img = newElem("img", imgPar, "songImg");
