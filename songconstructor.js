0dd3afcc69ba1aab59540a30edce2e798aab1e71
ItsHamty <contact.hamty@gmail.com>
2016-10-04 16:51:26 +0300

    Create songconstructor.js

---
+++ songconstructor.js
@@ -0,0 +1,198 @@
+function timeAgo(oldDate, length) {
+  length = length || 3;
+  var newDate = new Date();
+  var msInWeek = 1000 * 60 * 60 * 24 * 7,
+    msInDay = 1000 * 60 * 60 * 24;
+  var deltaYr = newDate.getFullYear() - oldDate.getFullYear();
+  if (oldDate.getMonth() > newDate.getMonth() ||
+    (oldDate.getMonth() === newDate.getMonth() && oldDate.getDate() > newDate.getDate())) {
+    deltaYr--;
+  }
+  var deltaMn = newDate.getMonth() - oldDate.getMonth();
+  if (deltaMn < 0) {
+    deltaMn += 12;
+  }
+  if (oldDate.getDate() > newDate.getDate()) {
+    deltaMn--;
+  }
+  var tempDate = new Date(oldDate.getFullYear() + deltaYr, oldDate.getMonth() + deltaMn, oldDate.getDate());
+  var tempDy = (newDate.getTime() - tempDate.getTime()) / msInDay;
+  var deltaDy = parseInt(tempDy);
+  var tempHr = (tempDy - deltaDy) * 24;
+  var deltaHr = parseInt(tempHr);
+  var tempMt = (tempHr - deltaHr) * 60;
+  var deltaMt = parseInt(tempMt);
+  var tempSc = (tempMt - deltaMt) * 60;
+  var deltaSc = parseInt(tempSc);
+  var sb = [];
+  if (deltaYr > 0) {
+    sb.push(deltaYr + ' year' + (deltaYr > 1 ? 's' : ''));
+  }
+  if (deltaMn > 0) {
+    sb.push(deltaMn + ' month' + (deltaMn > 1 ? 's' : ''));
+  }
+  if (deltaDy > 0) {
+    sb.push(deltaDy + ' day' + (deltaDy > 1 ? 's' : ''));
+  }
+  if (deltaHr > 0) {
+    sb.push(deltaHr + ' hour' + (deltaHr > 1 ? 's' : ''));
+  }
+  if (deltaMt > 0) {
+    sb.push(deltaMt + ' minute' + (deltaMt > 1 ? 's' : ''));
+  }
+  if (deltaSc > 0) {
+    sb.push(deltaSc + ' second' + (deltaSc > 1 ? 's' : ''));
+  }
+  if (sb.length > length) {
+    sb.length = length;
+  }
+  return sb.join(', ');
+}
+
+function addEvent(elem, evnt, func) {
+  if (elem.addEventListener) { // W3C DOM
+    elem.addEventListener(evnt, func, false);
+  } else if (elem.attachEvent) { // IE DOM
+    elem.attachEvent("on" + evnt, func);
+  } else { // No much to do
+    elem[evnt] = func;
+  }
+}
+String.prototype.isEmpty = function() {
+  return (this.length === 0 || !this.trim());
+};
+
+function Search(str) {
+  str = str || "";
+  if (str.isEmpty()) {
+    return Songs;
+  }
+  var srchTgs = str.split(" ");
+  for (var i = 0; i < srchTgs.length; i++) {
+    srchTgs[i] = srchTgs[i].toLowerCase();
+  }
+  var mObj = [];
+  for (var i = 0; i < Songs.length; i++) {
+    mObj[i] = true;
+  }
+  for (var i = 0; i < srchTgs.length; i++) {
+    for (var y = 0; y < mObj.length; y++) {
+      if (mObj[y]) {
+        if (Songs[y].name) {
+          if (((Songs[y].name).toLowerCase()).search(srchTgs[i]) == -1) {
+            delete mObj[y];
+          }
+        }
+      }
+    }
+  }
+  var results = [];
+  for (var i = 0; i < mObj.length; i++) {
+    if (mObj[i]) {
+      results.push(Songs[i]);
+    }
+  }
+  return results;
+}
+
+function newElem(type, parent, class1) {
+  var elem = document.createElement(type);
+  parent.appendChild(elem);
+  if (class1) {
+    elem.className = class1;
+  }
+  return elem;
+}
+
+function drawSongs(Songs) {
+  if (Songs) {} else {
+    return false;
+  }
+  var songParent = document.getElementById("SongParent");
+  songParent.innerHTML = "";
+  if (Songs.length == 0) {
+    return false;
+  }
+  for (i in Songs) {
+    var Song = Songs[i];
+    var elem = newElem("div", songParent, "SongElement");
+    if (Song.img) {
+      var imgPar = newElem("div", elem, "songImgPar")
+      var img = newElem("img", imgPar, "songImg");
+      img.src = Song.img;
+    }
+    var tal = newElem("div", elem, "TitleAndLinks");
+    tal.innerHTML += "<div class='songTitleDiv'><div class='songTitle'>" + Song.name + "</div></div>\n";
+    var links = newElem("div", tal, "LinkContainer");
+    if (Song.links.downloadFlac) {
+      var a = newElem("a", links, "link");
+      if (Song.links.downloadFlac.constructor === Array) {
+        a.href = Song.links.downloadFlac.join("");
+      } else {
+        a.href = Song.links.downloadFlac;
+      }
+      a.target = "_blank";
+      a.setAttribute("title", ("Free Download .flac (" + Song.name + ")"));
+      var dlbtn = newElem("img", a, "dlbtn");
+      dlbtn.src = "http://i.imgur.com/fbpGjDR.png";
+    }
+    if (Song.links.download) {
+      var a = newElem("a", links, "link");
+      if (Song.links.download.constructor === Array) {
+        a.href = Song.links.download.join("");
+      } else {
+        a.href = Song.links.download;
+      }
+      a.target = "_blank";
+      a.setAttribute("title", ("Free Download .mp3 (" + Song.name + ")"));
+      var dlbtn = newElem("img", a, "dlbtn");
+      dlbtn.src = "http://i.imgur.com/ra44DKt.png";
+    }
+    if (Song.links.soundcloud) {
+      var a = newElem("a", links, "link");
+      a.href = Song.links.soundcloud;
+      a.target = "_blank";
+      a.setAttribute("title", ("\"" + Song.name + "\" on Soundcloud"));
+      var dlbtn = newElem("img", a, "dlbtn");
+      dlbtn.src = "http://i.imgur.com/ybW9dgy.png";
+    }
+    if (Song.links.youtube) {
+      var a = newElem("a", links, "link");
+      a.href = Song.links.youtube;
+      a.target = "_blank";
+      a.setAttribute("title", ("\"" + Song.name + "\" on Youtube"));
+      var dlbtn = newElem("img", a, "dlbtn");
+      dlbtn.src = "http://i.imgur.com/qXf7WJw.png";
+    }
+    if (Song.links.bandcamp) {
+      var a = newElem("a", links, "link");
+      a.href = Song.links.bandcamp;
+      a.target = "_blank";
+      a.setAttribute("title", ("\"" + Song.name + "\" on Bandcamp"));
+      var dlbtn = newElem("img", a, "dlbtn");
+      dlbtn.src = "http://i.imgur.com/ic0KOox.png";
+    }
+    //Release Date
+    if (Song.date) {
+      if (Object.prototype.toString.call(Song.date) === "[object Date]") {
+        var date = Song.date;
+        var dates = newElem("div", elem, "DateContainer");
+        var date1 = newElem("div", dates, "dateText");
+        date1.innerHTML = date.toLocaleDateString([], {
+          day: "numeric",
+          month: "short",
+          year: "numeric"
+        });
+        date1.setAttribute("title", (date.toLocaleDateString([], {
+          day: "numeric",
+          month: "long",
+          year: "numeric"
+        }) + " | " + date.toLocaleTimeString([])));
+        var date2 = newElem("div", dates, "dateText");
+        date2.innerHTML = (timeAgo(date, 1) + " ago");
+        date2.setAttribute("title", (timeAgo(date)));
+      }
+    }
+  }
+  return true;
+}
