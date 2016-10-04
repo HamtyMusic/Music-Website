f93b61533d8ab468cdbfc34bddbcaaed72f975b1
ItsHamty <contact.hamty@gmail.com>
2016-10-04 16:20:22 +0300

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,79 +1,79 @@
-var Songs = [
-	{
-		name:"Lockyn - Aqua ( Hamty\'s Remix )",
-		img:"https://i.imgur.com/rNbn0Mn.jpg",
-		links:{
-			downloadFlac:"http://sh.st/HzIKA",
-			download:"http://sh.st/HzP5W",
-			soundcloud:"https://soundcloud.com/hamty/lockyn-aqua-remix",
-		youtube:"https://youtu.be/mEvwdNUg9jo"
-		},
-		date:new Date(2016,06,03)
-	},
-	{
-		name:"Hamty - Sunshine",
-		img:"http://i.imgur.com/YtgvHhF.jpg",
-		links:{
-			download:"http://sh.st/nqRuX",
-			soundcloud:"https://soundcloud.com/hamty/sunshine",
-			youtube:"https://youtu.be/Z7T-bCQcJVQ",
-			bandcamp:"https://hamty.bandcamp.com/track/sunshine"
-		},
-		date:new Date(2015,10,21)
-	},
-	{
-    name:"Hamty - Sea Lights",
-    img:"http://i.imgur.com/yzV6gD5.jpg",
-    links:{
-    download:["http://","adf",".ly/","1JcSyZ"],
-      soundcloud:"https://soundcloud.com/hamty/sea-lights",
-      youtube:"https://www.youtube.com/watch?v=UiGhXFMFY94",
-      bandcamp:"https://hamty.bandcamp.com/track/sea-lights"
+var Songs = {
+  lockynAqua: {
+    name: "Lockyn - Aqua ( Hamty\'s Remix )",
+    img: "https://i.imgur.com/rNbn0Mn.jpg",
+    links: {
+      downloadFlac: "http://sh.st/HzIKA",
+      download: "http://sh.st/HzP5W",
+      soundcloud: "https://soundcloud.com/hamty/lockyn-aqua-remix",
+      youtube: "https://youtu.be/mEvwdNUg9jo"
     },
-    date:new Date(2015,05,22,19)
-    },
-  {
-    name:"Hamty - Home",
-    img:"http://i.imgur.com/7KvqLCa.jpg",
-    links:{
-      download:["http://","adf",".ly/","1Bkfyo"],
-      soundcloud:"https://soundcloud.com/hamty/hamty-home",
-      youtube:"https://www.youtube.com/watch?v=Idmo2W8zcAg",
-      bandcamp:"https://hamty.bandcamp.com/track/home"
-    },
-    date:new Date(2015,02,25)
+    date: new Date(2016, 06, 03)
   },
-  {
-    name:"Hamty - Inspiration",
-		img:"http://i.imgur.com/IW0fpVX.jpg",
-		links:{
-			download:["http://","adf",".ly/","wTZVo"],
-			soundcloud:"https://soundcloud.com/hamty/hamty-inspiration",
-			youtube:"https://www.youtube.com/watch?v=Uo8Ozq5fZro",
-			bandcamp:"http://hamty.bandcamp.com/track/inspiration"
-		},
-		date:new Date(2015,00,20)
-	},
-	{
-		name:"Hamty - River Journey",
-		img:"http://i.imgur.com/SdQKV5Y.jpg",
-		links:{
-			download:["http://","adf",".ly/","sDPor"],
-			soundcloud:"https://soundcloud.com/hamty/hamty-home",
-			youtube:"https://www.youtube.com/watch?v=edFv3BTwk5g",
-			bandcamp:"https://hamty.bandcamp.com/track/river-journey"
-		},
-		date:new Date(2014,08,16)
-	},
-	{
-		name:"Hamty - The Return",
-		img:"http://i.imgur.com/yMuBKFT.jpg",
-		links:{
-			download:["http://","adf",".ly/","sDQXi"],
-			soundcloud:"https://soundcloud.com/hamty/the-return",
-			youtube:"https://www.youtube.com/watch?v=g5KG7cE5y44",
-			bandcamp:"https://hamty.bandcamp.com/track/the-return"
-		},
-		date:new Date(2014,06,17)
-	}
+  sunshine: {
+    name: "Hamty - Sunshine",
+    img: "http://i.imgur.com/YtgvHhF.jpg",
+    links: {
+      download: "http://sh.st/nqRuX",
+      soundcloud: "https://soundcloud.com/hamty/sunshine",
+      youtube: "https://youtu.be/Z7T-bCQcJVQ",
+      bandcamp: "https://hamty.bandcamp.com/track/sunshine"
+    },
+    date: new Date(2015, 10, 21)
+  },
+  seaLights: {
+    name: "Hamty - Sea Lights",
+    img: "http://i.imgur.com/yzV6gD5.jpg",
+    links: {
+      download: ["http://", "adf", ".ly/", "1JcSyZ"],
+      soundcloud: "https://soundcloud.com/hamty/sea-lights",
+      youtube: "https://www.youtube.com/watch?v=UiGhXFMFY94",
+      bandcamp: "https://hamty.bandcamp.com/track/sea-lights"
+    },
+    date: new Date(2015, 05, 22, 19)
+  },
+  home: {
+    name: "Hamty - Home",
+    img: "http://i.imgur.com/7KvqLCa.jpg",
+    links: {
+      download: ["http://", "adf", ".ly/", "1Bkfyo"],
+      soundcloud: "https://soundcloud.com/hamty/hamty-home",
+      youtube: "https://www.youtube.com/watch?v=Idmo2W8zcAg",
+      bandcamp: "https://hamty.bandcamp.com/track/home"
+    },
+    date: new Date(2015, 02, 25)
+  },
+  inspiration: {
+    name: "Hamty - Inspiration",
+    img: "http://i.imgur.com/IW0fpVX.jpg",
+    links: {
+      download: ["http://", "adf", ".ly/", "wTZVo"],
+      soundcloud: "https://soundcloud.com/hamty/hamty-inspiration",
+      youtube: "https://www.youtube.com/watch?v=Uo8Ozq5fZro",
+      bandcamp: "http://hamty.bandcamp.com/track/inspiration"
+    },
+    date: new Date(2015, 00, 20)
+  },
+  riverJourney: {
+    name: "Hamty - River Journey",
+    img: "http://i.imgur.com/SdQKV5Y.jpg",
+    links: {
+      download: ["http://", "adf", ".ly/", "sDPor"],
+      soundcloud: "https://soundcloud.com/hamty/hamty-home",
+      youtube: "https://www.youtube.com/watch?v=edFv3BTwk5g",
+      bandcamp: "https://hamty.bandcamp.com/track/river-journey"
+    },
+    date: new Date(2014, 08, 16)
+  },
+  return: {
+    name: "Hamty - The Return",
+    img: "http://i.imgur.com/yMuBKFT.jpg",
+    links: {
+      download: ["http://", "adf", ".ly/", "sDQXi"],
+      soundcloud: "https://soundcloud.com/hamty/the-return",
+      youtube: "https://www.youtube.com/watch?v=g5KG7cE5y44",
+      bandcamp: "https://hamty.bandcamp.com/track/the-return"
+    },
+    date: new Date(2014, 06, 17)
+  }
 ]
