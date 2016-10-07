609c4ee5f3be99ecd17d0140eb956c74feb26034
ItsHamty <contact.hamty@gmail.com>
2016-10-07 22:11:14 +0300

    Update songconstructor.js

--- songconstructor.js
+++ songconstructor.js
@@ -177,12 +177,14 @@ function drawSongs(Songs) {
         arrowBox.appendChild(a);
         dlbtn.className += " download-button flac";
         a.setAttribute("title", ("Free Download .flac (" + Song.name + ")"));
+        a.innerHTML += "Download .flac";
         dlbtn.src = "http://static.tumblr.com/mv8e1sl/otnoejhpo/flac.svg";
         dlbtn.onerror="this.onerror=null; this.src='http://static.tumblr.com/mv8e1sl/6ttoejhq2/flac.png'";
       } else if (key == "downloadMp3") {
         arrowBox.appendChild(a);
         dlbtn.className += " download-button mp3";
         a.setAttribute("title", ("Free Download .mp3 (" + Song.name + ")"));
+        a.innerHTML += "Download .mp3";
         dlbtn.src = "http://static.tumblr.com/mv8e1sl/NaQoejhqd/mp3.svg";
         dlbtn.onerror="this.onerror=null; this.src='http://static.tumblr.com/mv8e1sl/k2Moejhqh/mp3.png'";
       } else if (key == "soundcloud") {
