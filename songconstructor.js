114941e9b07c5bb8628be41f1378e38496d790bb
ItsHamty <contact.hamty@gmail.com>
2016-10-29 18:00:27 +0300

    Update songconstructor.js

--- songconstructor.js
+++ songconstructor.js
@@ -309,19 +309,16 @@ function drawSong(Song) {
     var ytid = Song.links.youtube.id;
     if(ytid) {
     if(window.curEmbed) {
-      if(window.curEmbed.dataset.id == ytid) {
-        
-      } else {
+      if(window.curEmbed.dataset.id != ytid) {
         window.curEmbed.outerHTML = "";
       }
-    } else {
-      var ytEmbedWrap = newElem("div", $("embeds"), "yt-embed-wrap embed-wrap");
-      var ytEmbed = newElem("iframe", ytEmbedWrap, "yt-embed embed");
-      ytEmbed.src = "https://www.youtube.com/embed/" + ytid + "?autoplay=1&origin=" + (location.href || (location + "") || location.pathname);
-      ytEmbed.setAttribute("frameborder", 0);
-      ytEmbedWrap.dataset.id = ytid;
-      window.curEmbed = ytEmbedWrap;
     }
+    var ytEmbedWrap = newElem("div", $("embeds"), "yt-embed-wrap embed-wrap");
+    var ytEmbed = newElem("iframe", ytEmbedWrap, "yt-embed embed");
+    ytEmbed.src = "https://www.youtube.com/embed/" + ytid + "?autoplay=1&origin=" + (location.href || (location + "") || location.pathname);
+    ytEmbed.setAttribute("frameborder", 0);
+    ytEmbedWrap.dataset.id = ytid;
+    window.curEmbed = ytEmbedWrap;
   }}
   
   return true;
