b0b1ad76a6b63a6f7e77eae266ee9bf7bc045548
ItsHamty <contact.hamty@gmail.com>
2016-11-26 21:12:48 +0200

    Update songconstructor.js

--- songconstructor.js
+++ songconstructor.js
@@ -183,7 +183,7 @@ function drawSongs(Songs) {
     var wrap = newElem("div", songParent, "song-wrap");
     var elem = newElem("div", wrap, "song pb shadow");
     if (Song.img) {
-      var imgPar = newElem("a", elem, "song-image-parent");
+      var imgPar = newElem("a", elem, "song-image-parent lighten");
       imgPar.href = "#" + i;
       var img = newElem("img", imgPar, "song-image shadow");
       img.src = Song.img.replace(/^http:\/\//i, 'https://');
