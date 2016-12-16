7cc52895a19a51d1d74927d250ecfa3e887c2ca7
ItsHamty <contact.hamty@gmail.com>
2016-12-16 14:44:59 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,114 @@
+function $(q) { return document.querySelectorAll(q) }
+String.prototype.capFirstLetter = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+}
+function setVectorSource(elem, id) {
+  if(elem && id) {
+    elem.src = images[id].svg;
+    elem.onerror="this.onerror=null; this.src=\'" + images[id].png + "\'";
+  } else {
+    return false;
+  }
+}
+var images = {
+  soundcloud: {
+    svg: "https://static.tumblr.com/mv8e1sl/50Soehftt/sc.svg",
+    png: "https://static.tumblr.com/mv8e1sl/48ioehftr/sc.png"
+  },
+  youtube: {
+    svg: "https://static.tumblr.com/mv8e1sl/g88oehfu7/yt.svg",
+    png: "https://static.tumblr.com/mv8e1sl/QnGoehfu3/yt.png"
+  },
+  bandcamp: {
+    svg: "https://static.tumblr.com/mv8e1sl/hNQoehftc/bc.svg",
+    png: "https://static.tumblr.com/mv8e1sl/Bpqoehft9/bc.png"
+  },
+  download: {
+    svg: "https://static.tumblr.com/mv8e1sl/SYGoeqq6l/dl2.svg",
+    png: "https://static.tumblr.com/mv8e1sl/tQKoeqq6o/dl2.png"
+  },
+  download2: {
+    svg: "http://static.tumblr.com/mv8e1sl/rRrogsjia/dl3.svg",
+    png: "http://static.tumblr.com/mv8e1sl/6aGogsjid/dl3.png"
+  }
+}
+function timeAgo(oldDate, length) {
+  length = length || 3;
+  var newDate = new Date();
+  if((newDate - oldDate) < 0) {
+    return "Coming soon";
+  }
+  var msInWeek = 1000 * 60 * 60 * 24 * 7,
+    msInDay = 1000 * 60 * 60 * 24;
+  var deltaYr = newDate.getFullYear() - oldDate.getFullYear();
+  if (oldDate.getMonth() > newDate.getMonth() ||
+    (oldDate.getMonth() === newDate.getMonth() && oldDate.getDate() > newDate.getDate())) {
+    deltaYr--;
+  }
+  var deltaMn = newDate.getMonth() - oldDate.getMonth();
+  if (deltaMn < 0) {
+    deltaMn += 12;
+  }
+  if (oldDate.getDate() > newDate.getDate()) {
+    deltaMn--;
+  }
+  var tempDate = new Date(oldDate.getFullYear() + deltaYr, oldDate.getMonth() + deltaMn, oldDate.getDate());
+  var tempDy = (newDate.getTime() - tempDate.getTime()) / msInDay;
+  var deltaDy = parseInt(tempDy);
+  var tempHr = (tempDy - deltaDy) * 24;
+  var deltaHr = parseInt(tempHr);
+  var tempMt = (tempHr - deltaHr) * 60;
+  var deltaMt = parseInt(tempMt);
+  var tempSc = (tempMt - deltaMt) * 60;
+  var deltaSc = parseInt(tempSc);
+  var sb = [];
+  if (deltaYr > 0) {
+    sb.push(deltaYr + ' year' + (deltaYr > 1 ? 's' : ''));
+  }
+  if (deltaMn > 0) {
+    sb.push(deltaMn + ' month' + (deltaMn > 1 ? 's' : ''));
+  }
+  if (deltaDy > 0) {
+    sb.push(deltaDy + ' day' + (deltaDy > 1 ? 's' : ''));
+  }
+  if (deltaHr > 0) {
+    sb.push(deltaHr + ' hour' + (deltaHr > 1 ? 's' : ''));
+  }
+  if (deltaMt > 0) {
+    sb.push(deltaMt + ' minute' + (deltaMt > 1 ? 's' : ''));
+  }
+  if (deltaSc > 0) {
+    sb.push(deltaSc + ' second' + (deltaSc > 1 ? 's' : ''));
+  }
+  if (sb.length > length) {
+    sb.length = length;
+  }
+  return (sb.join(', ') + " ago");
+}
+function newElem(type, parent, classes, id) {
+  parent = parent || document.body;
+  var elem = document.createElement(type);
+  parent.appendChild(elem);
+  if (classes) {
+    elem.className = classes;
+  }
+  if (id) {
+    elem.id = id;
+  }
+  return elem;
+}
+function addEvent(elem, evnt, func) {
+  if (elem.addEventListener) { // W3C DOM
+    elem.addEventListener(evnt, func, false);
+  } else if (elem.attachEvent) { // IE DOM
+    elem.attachEvent("on" + evnt, func);
+  } else { // No much to do
+    elem[evnt] = func;
+  }
+}
+String.prototype.isEmpty = function() {
+  return (this.length === 0 || !this.trim());
+};
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 function getCurrentMonth() {
   return (new Date()).getMonth()
